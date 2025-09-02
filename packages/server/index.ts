a095baffcb5cff0ae0b10293baf9b5c376d9b0ef
Chen Zhu <zhucoco414@gmail.com>
2025-09-02 16:02:54 -0700

    set up Prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import type {Request, Response} from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,15 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!'
-  );
+   res.send('Hello, World!');
 }); //response to root
 
 app.get('/api/hello', (req: Request, res: Response) => {
-  res.send({message: 'Hello, API!'});
+   res.send({ message: 'Hello, API!' });
 }); //response to root
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
