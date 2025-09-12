1c732e195333c14c40f1c6985f558e1b9200336e
Chen Zhu <zhucoco414@gmail.com>
2025-09-11 17:43:50 -0700

    able to track previous question

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -12,6 +12,7 @@ const client = new OpenAI({
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+let conversation = new Map<string, string>();
 
 app.get('/', (req: Request, res: Response) => {
    res.send(process.env.OPENAI_API_KEY);
@@ -22,14 +23,16 @@ app.get('/api/hello', (req: Request, res: Response) => {
 }); //response to root
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversation.get(conversationId),
    });
+   conversation.set(conversationId, response.id);
    res.json({ message: response.output_text });
 });
 
