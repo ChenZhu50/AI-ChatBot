c2e0d3d75b1dd909b4867729e99ad3bdac602d58
Chen Zhu <zhucoco414@gmail.com>
2025-09-12 14:18:11 -0700

    handling form submission

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,19 +1,52 @@
+import { useForm } from 'react-hook-form';
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 
+type FormData = {
+   prompt: string;
+};
+
 const ChatBot = () => {
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+   const onSubmit = (data: FormData) => {
+      console.log(data);
+      reset();
+   };
+
+   //when we take this onKeyDown function out
+   //we need to explicitly say what type is our e
+   //since inside form element, e is for sure a form event
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         handleSubmit(onSubmit)();
+         e.preventDefault();
+      }
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
          <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask me anything..."
             maxLength={1000}
          >
-            <Button className="rounded-full w-9 h-9">
+            <Button
+               disabled={!formState.isValid}
+               className="rounded-full w-9 h-9"
+            >
                <FaArrowUp />
             </Button>
          </textarea>
-      </div>
+      </form>
    );
 };
 
