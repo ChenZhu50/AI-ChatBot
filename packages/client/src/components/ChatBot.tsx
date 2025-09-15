bc407a763feeabfa3e5dfdaad72b5a476d7ef475
Chen Zhu <zhucoco414@gmail.com>
2025-09-15 10:03:32 -0700

    connected to back end

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { useRef, type KeyboardEvent } from 'react';
-import { useForm } from 'react-hook-form';
+import { useRef, type KeyboardEvent, useState } from 'react';
+import { set, useForm } from 'react-hook-form';
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 
@@ -8,17 +8,23 @@ type FormData = {
    prompt: string;
 };
 
+type ChatResponse = {
+   message: string;
+};
+
 const ChatBot = () => {
+   const [message, setMessage] = useState<string[]>([]);
    const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
    const onSubmit = async ({ prompt }: FormData) => {
+      setMessage((prev) => [...prev, prompt]);
       reset();
-      const { data } = await axios.post('/api/chat', {
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
          prompt,
          conversationId: conversationId.current,
       });
-      console.log(data);
+      setMessage((prev) => [...prev, data.message]);
    };
 
    //when we take this onKeyDown function out
@@ -32,28 +38,35 @@ const ChatBot = () => {
    };
 
    return (
-      <form
-         onSubmit={handleSubmit(onSubmit)}
-         onKeyDown={onKeyDown}
-         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-      >
-         <textarea
-            {...register('prompt', {
-               required: true,
-               validate: (data) => data.trim().length > 0,
-            })}
-            className="w-full border-0 focus:outline-0 resize-none"
-            placeholder="Ask me anything..."
-            maxLength={1000}
+      <div>
+         <div>
+            {message.map((msg, index) => (
+               <p key={index}>{msg}</p>
+            ))}
+         </div>
+         <form
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
          >
-            <Button
-               disabled={!formState.isValid}
-               className="rounded-full w-9 h-9"
+            <textarea
+               {...register('prompt', {
+                  required: true,
+                  validate: (data) => data.trim().length > 0,
+               })}
+               className="w-full border-0 focus:outline-0 resize-none"
+               placeholder="Ask me anything..."
+               maxLength={1000}
             >
-               <FaArrowUp />
-            </Button>
-         </textarea>
-      </form>
+               <Button
+                  disabled={!formState.isValid}
+                  className="rounded-full w-9 h-9"
+               >
+                  <FaArrowUp />
+               </Button>
+            </textarea>
+         </form>
+      </div>
    );
 };
 
