0f10430d679496f60d97852a1477d3711d4feef5
Chen Zhu <zhucoco414@gmail.com>
2025-09-22 12:37:31 -0700

    get review router added

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { chatController } from './controllers/chat.controller';
+import { Prisma } from './generated/prisma';
+import { PrismaClient } from './generated/prisma';
+import { is } from 'zod/locales';
 
 const routes = express.Router();
 
@@ -14,5 +17,21 @@ routes.get('/api/hello', (req: Request, res: Response) => {
 }); //response to root
 
 routes.post('/api/chat', chatController.sendMessage);
+
+routes.get('/api/products/:id/reviews', async (req: Request, res: Response) => {
+   const prisma = new PrismaClient();
+   const productId = Number(req.params.id);
+
+   if (isNaN(productId)) {
+      return res.status(400).json({ error: 'Invalid product ID' });
+   }
+
+   const reviews = await prisma.review.findMany({
+      where: { productID: productId },
+      orderBy: { createdAt: 'desc' },
+   });
+
+   res.json(reviews);
+});
 
 export default routes; //since this the only object in this file, we can use default export
